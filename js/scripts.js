7427fa25791937aae717181f01768e6a24f03eaa
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-01-17 09:45:44 -0800

    add branching and work on debugging it

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,18 @@
 $(document).ready(function(){
   $("form#form").submit(function(){
-    event.preventDefault();
-    var favoriteAnimal = $("select#favoriteAnimal");
-    var favoriteColor = $("select#favoriteColor");
-    var vacation = $("select#vacation");
-    var languageType = $("select#languageType");
-    var languageType = $("select#languageType");
+   
+    var favoriteAnimal = $("select#favoriteAnimal").val();
+    var favoriteColor = $("select#favoriteColor").val();
+    var vacation = $("select#vacation").val();
+    var languageType = $("select#languageType").val();
+    var personType = $("select#personType").val();
 
-  
+   
+    if (favoriteAnimal === 'snake' && vacation === 'beach' &&languageType !== 'loose') {
+      alert("You should learn Python!");
+    } else {
+      alert("You should learn all");
+    }
+    event.preventDefault();
   });
 });
