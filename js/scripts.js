9a590f863c050bbe6cee18f592c52bfbe33eef8c
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-01-17 09:21:09 -0800

    create js variables and add output div

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
   $("form#form").submit(function(){
     event.preventDefault();
-    var favoriteAnimal=$("select#favoriteAnimal");
+    var favoriteAnimal = $("select#favoriteAnimal");
+    var favoriteColor = $("select#favoriteColor");
+    var vacation = $("select#vacation");
+    var languageType = $("select#languageType");
+    var languageType = $("select#languageType");
+
+  
   });
 });
