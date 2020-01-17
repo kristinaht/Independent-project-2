43d7b4a3bde516c3314315223c29a80f593d89f2
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-01-17 09:59:23 -0800

    find bug and correct js to reference form class instead of id

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
 $(document).ready(function(){
-  $("form#form").submit(function(){
+  $("form.form").submit(function(){
    
     var favoriteAnimal = $("select#favoriteAnimal").val();
     var favoriteColor = $("select#favoriteColor").val();
-    var vacation = $("select#vacation").val();
-    var languageType = $("select#languageType").val();
-    var personType = $("select#personType").val();
+    // var vacation = $("select#vacation").val();
+    // var languageType = $("select#languageType").val();
+    // var personType = $("select#personType").val();
 
    
-    if (favoriteAnimal === 'snake' && vacation === 'beach' &&languageType !== 'loose') {
+    if (favoriteAnimal === "snake" && favoriteColor === "green") {
       alert("You should learn Python!");
     } else {
       alert("You should learn all");
