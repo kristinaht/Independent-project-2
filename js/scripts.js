819e258d6aba524c0babb418dc8027fd84d91920
Kristina Hengster Tintor <hengster.tintor@gmail.com>
2020-01-17 10:17:08 -0800

    modify else if statements

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,17 @@ $(document).ready(function(){
    
     var favoriteAnimal = $("select#favoriteAnimal").val();
     var favoriteColor = $("select#favoriteColor").val();
-    // var vacation = $("select#vacation").val();
-    // var languageType = $("select#languageType").val();
-    // var personType = $("select#personType").val();
+    var vacation = $("select#vacation").val();
+    var languageType = $("select#languageType").val();
+    var personType = $("select#personType").val();
 
    
-    if (favoriteAnimal === "snake" && favoriteColor === "green") {
-      alert("You should learn Python!");
-    } else {
+    if (favoriteAnimal === "snake" && favoriteColor === "green" & languageType !== "loose") {
+      $("#output").text("You should learn Python!");
+    } else if (vacation === "beach" && favoriteColor === "red" || personType === "owl") {
+      $("#output").text("You should learn JavaScript!");
+    }
+    else {
       alert("You should learn all");
     }
     event.preventDefault();
